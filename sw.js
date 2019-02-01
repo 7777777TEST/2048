8c1e53454d15f00028a21c0084c31f045cec8793
YUTA <YUTA@gmail.com>
2019-02-01 22:40:01 +0900

    Init project

---
+++ sw.js
@@ -0,0 +1,41 @@
+var CACHE_NAME = '2048_caches';
+var appShellFiles = [
+  '/2048/',
+  '/2048/index.html',
+  '/2048/images/app.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(appShellFiles);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request).then(function(response) {
+        return caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (CACHE_NAME !== cacheName &&  cacheName.startsWith("pinball-caches")) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
